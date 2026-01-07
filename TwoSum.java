a982c924295501ac17785bc087e805799e0ebb6a
pranav <pranavshinde150g@gmail.com>
2026-01-07 07:42:26 +0530

    TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+class TwoSum {
+    public static void main(String args[]) {
+        int arr[] = { 10, 20, 30, 40, 50 };
+        int target = 50;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("Found " + arr[i] + " " + arr[j]);
+                }
+            }
+        }
+    }
+}
