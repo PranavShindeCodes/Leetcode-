67ef0c3cc2728f3716e758a145d21292391f0320
Pranav Shinde <pranavshinde150g@gmail.com>
2026-01-10 18:02:44 +0530

    3rd

---
+++ LargestNumber.java
@@ -0,0 +1,13 @@
+class Main{
+  public static void main(String args[]}{
+  int arr[]={10,20,30,40};
+  int max=arr[0];
+  for(int i=0;i<arr.length;i++)
+    {
+      if(arr[i]>max){
+        max=arr[i];
+      }
+    }
+  
+  }
+}
